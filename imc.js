24a6e11bdf0242daad192f8aa4adb5b36ecdf848
Breno Silva <breno.csilva@hotmail.com>
2021-08-10 10:17:19 -0300

    Usando else if - criando calculadora imc

--- imc.js
+++ imc.js
@@ -1,9 +1,35 @@
-const nome = document.getElementById('nome');
-const altura = document.getElementById('altura');
-const peso = document.getElementById('peso');
+
 const calcular = document.getElementById('calcular');
-const resultado = document.getElementById('resultado');
-
-alert(nome)
 
 
+function imc(){
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if(nome !== '' && altura != '' && peso != ''){
+        const valorIMC = (peso / (altura*altura)).toFixed(0);
+
+        let classificacao = "";
+
+        if(valorIMC < 18.5){
+            classificacao = 'abaixo do peso.'
+        }else if(valorIMC < 25 ){
+            classificacao = 'com peso ideal. Parabens'
+        }else if(valorIMC < 35){
+            classificacao = 'com obesidade grau 1'
+        }else if(valorIMC <40){
+            classificacao = 'com obsedidade grau 2'
+        }else{
+            classificacao = 'com obesidade grau 3, ja morreu'
+        }
+
+        resultado.textContent = `${nome} your IMC is ${valorIMC} and you is ${classificacao}` ;
+    }else{
+        resultado.textContent = 'Preencha todos os campos'
+    }
+
+}
+
+calcular.addEventListener('click', imc);
